f1978a1e1fca0c6544957dc7e128ea307da6adeb
Nmesoma Onumaegbu <nmesoma.onumaegbu1@ontariotechu.net>
2025-12-11 15:14:23 -0500

    changed port number

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,7 @@ import { connectDB } from "./config/db";
 import app from "./app";
 import http from "http";
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5050;
 
 (async() => {
     try{
