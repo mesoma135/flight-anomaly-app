6374d8fbb5df99912f17513424418ba7ee541bd0
mesoma135 <146199540+mesoma135@users.noreply.github.com>
2025-12-12 15:45:49 -0500

    mapped data fetched from openskyapi

--- backend/dist/controllers/openSkyAPIController.js
+++ backend/dist/controllers/openSkyAPIController.js
@@ -18,7 +18,23 @@ const openSkyAPI = process.env.OPENSKY_URL || "";
 const fetchAllStates = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const response = yield axios_1.default.get(`${openSkyAPI}/states/all`, { timeout: 10000 });
-        res.json(response.data);
+        const mappedStates = response.data.states.map((flight) => {
+            var _a, _b;
+            return ({
+                callsign: flight[1] ? flight[1].trim() : "",
+                origin_country: flight[2] || "",
+                longitude: (_a = flight[5]) !== null && _a !== void 0 ? _a : null,
+                latitude: (_b = flight[6]) !== null && _b !== void 0 ? _b : null,
+                speed: flight[9] != null ? Math.floor(flight[9] * 1.94384) : null, //converting from m/s to kts
+                heading: flight[10] != null ? Math.floor(flight[10]) : null,
+                verticalSpeed: flight[11] != null ? Math.floor(flight[11] * 196.85) : null, //converting from m/s to ft/min
+                altitude: flight[13] != null ? Math.floor(flight[13] * 3.28084) : null,
+            });
+        });
+        res.json({
+            count: mappedStates.length,
+            states: mappedStates
+        });
         console.log("Flights recieved: ", response.data.states.length);
     }
     catch (error) {
--- backend/src/controllers/openSkyAPIController.ts
+++ backend/src/controllers/openSkyAPIController.ts
@@ -8,7 +8,21 @@ export const fetchAllStates = async (req: Request, res: Response) =>{
         const response = await axios.get(
             `${openSkyAPI}/states/all`, { timeout: 10000 }
         );
-        res.json(response.data);
+
+        const mappedStates = response.data.states.map((flight: any[]) => ({
+            callsign: flight[1] ? flight[1].trim(): "",
+            origin_country: flight[2] || "",
+            longitude: flight[5] ?? null,
+            latitude: flight[6] ?? null,
+            speed: flight[9] != null ? Math.floor(flight[9]*1.94384): null, //converting from m/s to kts
+            heading: flight[10] != null ? Math.floor(flight[10]): null,
+            verticalSpeed: flight[11] != null ? Math.floor(flight[11]*196.85): null, //converting from m/s to ft/min
+            altitude: flight[13] != null ? Math.floor(flight[13]*3.28084): null,
+          }));
+          res.json({
+            count: mappedStates.length,
+            states: mappedStates
+          })
         console.log("Flights recieved: ", response.data.states.length);
     }
     catch(error){
